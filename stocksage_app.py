204caab6adee2ea073ca3458ad867b2c5e279a42
Ayush Saini <120465410+AyushSaini0808@users.noreply.github.com>
2024-07-26 22:01:59 +0530

    Update stocksage_app.py

--- stocksage_app.py
+++ stocksage_app.py
@@ -38,7 +38,7 @@ title=st.markdown('''
          # *StockSage* : **An Intelligent Stock Price Predictor**''')
 st.divider()
 about=st.markdown("### About the Project:")
-para1=st.sidebar.info("""StockSage is designed to provide insightful information regarding the stock trends of some of the major tech. companies. Utilizing LSTM(Long-Short Term Memory),a recurrent neural network, StockSage analyzes historical stock data to forecast future price movements with high accuracy.
+para1=st.sidebar.info("""StockSage is designed to provide insightful information regarding the stock trends of some of the major tech. companies. Utilizing LSTM(Long-Short Term Memory),a recurrent neural network, StockSage analyzes historical stock data to forecast future price movements with high accuracy.""")
 
    
    
